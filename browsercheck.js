8a54cd164700bb8c0df0b7aab74428fe1c6d9502
_tnull <trash@tnull.de>
2013-09-20 16:20:35 +0200

    Erste testmethoden ... viele TODOs!

--- browsercheck.js
+++ browsercheck.js
@@ -1,5 +1,3 @@
-
-
 // BrowserDetect von http://www.quirksmode.org/js/detect.html
 var BrowserDetect = {
 	init: function () {
@@ -119,5 +117,149 @@ var BrowserDetect = {
 };
 BrowserDetect.init();
 
-console.log("browser: " + BrowserDetect.browser);
-console.log("version: " + BrowserDetect.version);
+browser = BrowserDetect.browser;
+version = BrowserDetect.version;
+document.write("browser: " + browser + "<br />");
+document.write("version: " + version + "<br />");
+
+
+
+
+testCrime(browser, version); 
+
+function testCrime(browser, version) {
+	//console.log("Testing for Crime...");
+	vulnerable = false;
+	switch(browser) {
+		// MSIE nicht verwundbar
+		case "Chrome":
+			if(version < 21.0) vulnerable = true;
+			break;
+		case "Safari":
+			if(version < 5.1) vulnerable = true;
+			break;
+		case "Firefox":
+			if(version < 15.0) vulnerable = true;
+			break;
+		case "Opera":
+			if(version < 21.01) vulnerable = true;
+			break;
+
+		default:
+			break;
+	}
+	if(vulnerable) document.write("your browser is vulnerable to CRIME");
+	else document.write("your browser doesn't seem vulnerable to CRIME");
+
+	return vulnerable;
+}
+function testBeast(browser, version) {
+	vulnerable = false;
+	switch(browser) {
+		case "Chrome":
+			if(version < 16.0) vulnerable = true;
+			break;
+		case "MSIE":
+			//TODO: welche version??
+			break;
+		case "Safari":
+			//TODO: welche version??
+			break;
+		case "Firefox":
+			if(version < 10.0) vulnerable = true;
+			break;
+		case "Opera":
+			//TODO: welche version??
+			break;
+
+		default:
+			break;
+	}
+	if(vulnerable) document.write("your browser is vulnerable to BEAST");
+	else document.write("your browser doesn't seem vulnerable to BEAST");
+
+	return vulnerable;
+
+}
+function testBreach(browser, version) {
+	vulnerable = false;
+	switch(browser) {
+		case "Chrome":
+			//TODO: welche version??
+			break;
+		case "MSIE":
+			//TODO: welche version??
+			break;
+		case "Safari":
+			//TODO: welche version??
+			break;
+		case "Firefox":
+			//TODO: welche version??
+			break;
+		case "Opera":
+			//TODO: welche version??
+			break;
+
+		default:
+			break;
+	}
+	if(vulnerable) document.write("your browser is vulnerable to BREACH");
+	else document.write("your browser doesn't seem vulnerable to BREACH");
+
+	return vulnerable;
+}
+
+function testLucky13(browser, version) {
+	vulnerable = false;
+	switch(browser) {
+		case "Chrome":
+			//TODO: welche version??
+			break;
+		case "MSIE":
+			//TODO: welche version??
+			break;
+		case "Safari":
+			//TODO: welche version??
+			break;
+		case "Firefox":
+			//TODO: welche version??
+			break;
+		case "Opera":
+			if(version < 12.13) vulnerable = true;
+			break;
+
+		default:
+			break;
+	}
+	if(vulnerable) document.write("your browser is vulnerable to Lucky13");
+	else document.write("your browser doesn't seem vulnerable to Lucky13");
+
+	return vulnerable;
+}
+function testTime(browser, version) {
+	vulnerable = false;
+	switch(browser) {
+		case "Chrome":
+			if(version < 4.1) vulnerable = true;
+			break;
+		case "MSIE":
+			if(version < 8.0) vulnerable = true;
+			break;
+		case "Safari":
+			if(version < 4.0) vulnerable = true;
+			break;
+		case "Firefox":
+			if(version < 3.6) vulnerable = true;
+			break;
+		case "Opera":
+			if(version < 10.5) vulnerable = true;
+			break;
+
+		default:
+			break;
+	}
+	if(vulnerable) document.write("your browser is vulnerable to TIME");
+	else document.write("your browser doesn't seem vulnerable to TIME");
+
+	return vulnerable;
+}
