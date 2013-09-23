4524690abc38619161730bcb30d5aa3a1ab3d0fc
_tnull <trash@tnull.de>
2013-09-23 15:04:34 +0200

    Div ist so gut wie fertig

--- www/wasuptls.js
+++ www/wasuptls.js
@@ -16,7 +16,7 @@ function PHPtoJSResponse(data) {
 
    // Testing Forward Secrecy
    var usingFS;
-   var m = cipher.match(/^ECDH-|^DH-/g);
+   var m = cipher.match(/^ECDH|^DH/g);
    if (m == null) usingFS = false; 
    else usingFS = true; 
 
@@ -32,5 +32,20 @@ function PHPtoJSResponse(data) {
    if (m == null) usingSSLv3 = false; 
    else usingSSLv3 = true; 
 
-   
+   if(!usingFS || usingRC4 || usingSSLv3) {
+    	var popup = '<div style="position: fixed; width: 100%; height: 100px; background-color: #3366CC; color: white; padding: 10px; bottom: 0; left:0; font-family: Helvetic, sans-serif; font-size: 12pt; box-shadow: 0 0 10px #000000 ">';
+	
+	   popup += "Your HTTPS connections doesn't use the up-to-date standards and might therefore be unsecure! Please update your browser to the newest version!<br /><ul>";
+	   if (!usingFS) {
+		   popup += '<li>No <a href="http://en.wikipedia.org/wiki/Forward_secrecy">Forward Secrecy</a> is used!</li>";
+	   }
+	   if (usingRC4) {
+		   popup += '<li>The <a href="http://en.wikipedia.org/wiki/Rc4">old RC4 encryption standard</a> is used!</li>';
+	   }
+	   if (usingSSLv3) {
+		   popup += '<li>The <a href="http://en.wikipedia.org/wiki/Transport_Layer_Security#SSL_3.0">old SSLv3 standard</a> is used!</li>';
+	   }
+	   popup += "</ul></div>";
+	   document.write(popup);
+   }
 };
