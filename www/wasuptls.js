878ccaf16dc11ab3903eb86c0f6940243b0b3ef1
_tnull <trash@tnull.de>
2013-09-23 14:15:48 +0200

    - Tests für RC4 / SSLv3 / FS - Browsercheck.js gelöscht

--- www/wasuptls.js
+++ www/wasuptls.js
@@ -11,5 +11,26 @@ function PHPtoJSResponse(data) {
    // Antwort des Server ggf. verarbeiten
    var response = $.parseJSON(data);
    var cipher = response.cipher;
+   var version = response.version;
    alert("TLS-Version: " + response.version + "\nCipher: " + response.cipher + "\nKeysize: " + response.keysize);
+
+   // Testing Forward Secrecy
+   var usingFS;
+   var m = cipher.match(/^ECDH-|^DH-/g);
+   if (m == null) usingFS = false; 
+   else usingFS = true; 
+
+   // Test for RC4
+   var usingRC4;
+   var m = cipher.match(/*RC4*/g);
+   if (m == null) usingRC4 = false; 
+   else usingRC4 = true; 
+
+   // Test for SSLv3
+   var usingSSLv3;
+   var m = version.match(/*SSLv3*/g);
+   if (m == null) usingSSLv3 = false; 
+   else usingSSLv3 = true; 
+
+   
 };
